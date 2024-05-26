122395d208e442629f210538b32c4b38d8bd2fef
고태경 <202244042@itc.ac.kr>
2024-05-27 02:38:21 +0900

    완성

--- ServerProgramming/src/main/java/kr/co/inhatcspring/mapper/MapperInterface.java
+++ ServerProgramming/src/main/java/kr/co/inhatcspring/mapper/MapperInterface.java
@@ -9,7 +9,6 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import kr.co.inhatcspring.beans.BoardDataBean;
-import kr.co.inhatcspring.beans.DataBean;
 
 public interface MapperInterface {
 	@Insert("INSERT INTO ONLINEBOARD (BOARDID, CATEGORY, TITLE, CONTENT, USERID) VALUES (board_seq.NEXTVAL, #{category}, #{title}, #{content}, #{userId})")
@@ -26,12 +25,4 @@ public interface MapperInterface {
 
     @Delete("DELETE FROM ONLINEBOARD WHERE BOARDID = #{boardId}")
     void deleteBoardData(@Param("boardId") Long boardId);
-
-
-	// 12주차
-	@Insert("insert into spring_mvc_table (data1, data2, data3) values (#{data1}, #{data2}, #{data3} )")
-	void insert_data(DataBean dataBean);
-	
-	@Select("select data1, data2, data3 from spring_mvc_table")
-	List<DataBean> select_data();
 }
