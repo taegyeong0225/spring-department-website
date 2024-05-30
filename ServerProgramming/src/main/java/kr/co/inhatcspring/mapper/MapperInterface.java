6f4248c8fa3162ee111d43457aa2a5646de37bb7
고태경 <202244042@itc.ac.kr>
2024-05-30 09:11:11 +0900

    backup

--- ServerProgramming/src/main/java/kr/co/inhatcspring/mapper/MapperInterface.java
+++ ServerProgramming/src/main/java/kr/co/inhatcspring/mapper/MapperInterface.java
@@ -9,7 +9,6 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import kr.co.inhatcspring.beans.BoardDataBean;
-import kr.co.inhatcspring.beans.DataBean;
 
 public interface MapperInterface {
     
@@ -34,12 +33,4 @@ public interface MapperInterface {
     // 특정 게시글 ID의 데이터 삭제
     @Delete("DELETE FROM ONLINEBOARD WHERE BOARDID = #{boardId}")
     void deleteBoardData(@Param("boardId") Long boardId);
-
-
-	// 12주차
-	@Insert("insert into spring_mvc_table (data1, data2, data3) values (#{data1}, #{data2}, #{data3} )")
-	void insert_data(DataBean dataBean);
-	
-	@Select("select data1, data2, data3 from spring_mvc_table")
-	List<DataBean> select_data();
 }
